a123b3f27beb552384afdb2bf494cad316e2e925
brunoadsba <brunoadsba@gmail.com>
2025-04-27 21:50:51 -0300

    Corrigido startup.sh para garantir que o gunicorn seja instalado

--- startup.sh
+++ startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Script de inicialização para o Render
 
+# Garantir que o gunicorn esteja instalado
+pip install gunicorn
+
 # Criar diretórios necessários
 mkdir -p pdfs
 chmod 755 pdfs
 
 # Executar o aplicativo
-exec gunicorn app:app --bind=0.0.0.0:$PORT --log-file=- 
+exec python -m gunicorn app:app --bind=0.0.0.0:$PORT --log-file=- 
